7d7fcd5e217500c9b5740eff450622df3b3a13ef
elisatodd <elitodd@ucm.es>
2023-05-12 18:00:47 +0200

    Fin práctica 2
    Co-Authored-By: Miguel González Pérez <82326243+miggon23@users.noreply.github.com>

--- practica2.2/replicacion-chat/Chat.cc
+++ practica2.2/replicacion-chat/Chat.cc
@@ -5,21 +5,17 @@
 
 void ChatMessage::to_bin()
 {
-    alloc_data(MESSAGE_SIZE);
+     alloc_data(MESSAGE_SIZE);
+
+    memset(_data, 0, MESSAGE_SIZE);
 
     //Serializar los campos type, nick y message en el buffer _data
-
-    // Serializar el campo type
-    char* tmp = _data;
-    memcpy(tmp, &type, sizeof(uint8_t));
-    tmp += sizeof(uint8_t);
-
-    // Serializar el campo nick
-    memcpy(tmp, nick.c_str(), nick.length() + 1);
-    tmp += sizeof(char) * 8;
-
-    // Serializar el campo message
-    memcpy(tmp, message.c_str(), message.length() + 1);
+    char* tmp=_data;
+    memcpy(_data, &type, sizeof(uint8_t));
+    tmp += nick.length() + 1;
+    memcpy(_data, nick.c_str(), sizeof(char)*8);
+    tmp += nick.length() + 1;
+    memcpy(_data, message.c_str(), sizeof(char)*80);
 }
 
 int ChatMessage::from_bin(char * bobj)
@@ -59,64 +55,38 @@ void ChatServer::do_messages()
 {
 
     while (true) {
-        /*
-        * NOTA: los clientes están definidos con "smart pointers", es necesario
-        * crear un unique_ptr con el objeto socket recibido y usar std::move
-        * para añadirlo al vector
-        */
+                /*
+         * NOTA: los clientes están definidos con "smart pointers", es necesario
+         * crear un unique_ptr con el objeto socket recibido y usar std::move
+         * para añadirlo al vector
+         */
 
-        // Esperar a que llegue un mensaje de algún cliente
-        Socket *client_socket = socket.recv();
-        
-        // Recibir mensaje
-        std::string message;
-        try {
-            message = client_socket->recv_string();
-        } catch (const std::exception &e) {
-            // Si se produce un error al recibir el mensaje, se cierra la conexión con el cliente
-            std::cout << "Error al recibir mensaje: " << e.what() << std::endl;
-            remove_client(client_socket);
-            continue;
+        //Recibir Mensajes en y en función del tipo de mensaje
+        // - LOGIN: Añadir al vector clients
+        // - LOGOUT: Eliminar del vector clients
+        // - MESSAGE: Reenviar el mensaje a todos los clientes (menos el emisor)
+        Socket* clientSd;
+        ChatMessage msg;
+        //Recibir Mensajes en y en función del tipo de mensaje
+        socket.recv(msg, clientSd);
+
+        if(msg.type == ChatMessage::MessageType::LOGIN){
+            std::cout << msg.nick << " LOGIN\n";
+            clients.push_back(std::unique_ptr<Socket>(std::move(clientSd)));
         }
-
-        // Determinar el tipo de mensaje recibido
-        Message message_type = get_message_type(message);
-
-        // Realizar acciones en función del tipo de mensaje recibido
-        switch (message_type) {
-            case Message::LOGIN: {
-                // Añadir al vector clients
-                add_client(client_socket);
-                std::cout << "Nuevo cliente conectado." << std::endl;
-                break;
-            }
-            case Message::LOGOUT: {
-                // Eliminar del vector clients
-                remove_client(client_socket);
-                std::cout << "Cliente desconectado." << std::endl;
-                break;
-            }
-            case Message::MESSAGE: {
-                // Reenviar el mensaje a todos los clientes (menos el emisor)
-                std::cout << "Mensaje recibido: " << message << std::endl;
-                for (auto &client : clients) {
-                    if (client.get() != client_socket) {
-                        try {
-                            client->send_string(message);
-                        } catch (const std::exception &e) {
-                            // Si se produce un error al enviar el mensaje, se cierra la conexión con el cliente
-                            std::cout << "Error al enviar mensaje: " << e.what() << std::endl;
-                            remove_client(client.get());
-                        }
-                    }
-                }
-                break;
-            }
-            case Message::UNKNOWN: {
-                // Si se recibe un mensaje desconocido, se cierra la conexión con el cliente
-                std::cout << "Mensaje desconocido recibido: " << message << std::endl;
-                remove_client(client_socket);
-                break;
+        // - LOGOUT: Eliminar del vector clients
+        else if(msg.type == ChatMessage::MessageType::LOGOUT){
+            std::cout << msg.nick << " LOGOUT\n";
+            auto it = clients.begin();
+            while(it != clients.end() && !(*(*it).get() == *clientSd)) it++;
+            clients.erase(it);
+        }
+        // - MESSAGE: Reenviar el mensaje a todos los clientes (menos el emisor)
+        else if(msg.type == ChatMessage::MessageType::MESSAGE){
+            std::cout << msg.nick << " MESSAGE\n";
+            for(int i=0; i<clients.size(); ++i){
+                if(!(*(clients[i].get()) == *clientSd))
+                    socket.send(msg, (*clients[i].get()));
             }
         }
     }
@@ -152,6 +122,11 @@ void ChatClient::input_thread()
         std::string msg;
         std::getline(std::cin, msg);
 
+        if(msg=="q"){
+            logout();
+            break;
+        }
+        
         ChatMessage em(nick, msg);
         em.type = ChatMessage::MESSAGE;
 
@@ -166,8 +141,8 @@ void ChatClient::net_thread()
     {
         //Recibir Mensajes de red
         ChatMessage msg;
-        socket.recv(msg, socket);
-
+        socket.recv(msg);
+        
         //Mostrar en pantalla el mensaje de la forma "nick: mensaje"
         if (msg.nick != nick)
         {
